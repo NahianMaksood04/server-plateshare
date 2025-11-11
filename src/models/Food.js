b18ba1b2a8dbfb67a8552e43916cf442172be8c1
NahianMaksood04 <nahianmaksood04@gmail.com>
2025-11-11 15:50:42 +0600

    food and request

---
+++ src/models/Food.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { FOOD_STATUS } = require("../utils/constants");
+
+const FoodSchema = new mongoose.Schema(
+  {
+    foodName: { type: String, required: true, trim: true },
+    foodImage: { type: String, required: true },
+    foodQuantity: { type: Number, required: true, min: 1 },
+    pickupLocation: { type: String, required: true },
+    expireDate: { type: Date, required: true },
+    notes: { type: String, default: "" },
+
+    donorName: { type: String, required: true },
+    donorEmail: { type: String, required: true, index: true },
+    donorPhoto: { type: String, default: "" },
+
+    food_status: {
+      type: String,
+      enum: Object.values(FOOD_STATUS),
+      default: FOOD_STATUS.AVAILABLE,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Food", FoodSchema);
---
+++ src/models/Request.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { REQUEST_STATUS } = require("../utils/constants");
+
+const RequestSchema = new mongoose.Schema(
+  {
+    food: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Food",
+      required: true,
+      index: true,
+    },
+
+    userName: { type: String, required: true },
+    userEmail: { type: String, required: true, index: true },
+    userPhoto: { type: String, default: "" },
+
+    location: { type: String, required: true },
+    reason: { type: String, required: true },
+    contactNo: { type: String, required: true },
+
+    status: {
+      type: String,
+      enum: Object.values(REQUEST_STATUS),
+      default: REQUEST_STATUS.PENDING,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Request", RequestSchema);
